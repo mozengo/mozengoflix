22906cb96c62345328b609cbd8714ed49259e9f2
mozengo <outro471@gmail.com>
2025-06-26 22:08:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ function filtrar() {
 }
 
 window.onload = renderizarCatalogo;
+function fazerLogin() {
+  const role = document.getElementById("role").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("mensagem");
+
+  if (!username || !password) {
+    msg.textContent = "Preencha todos os campos.";
+    return;
+  }
+
+  if (role === "usuario" && username === "usuario" && password === "1234") {
+    window.location.href = "home_usuario.html";
+  } else if (role === "criador" && username === "criador" && password === "admin") {
+    window.location.href = "painel_criador.html";
+  } else {
+    msg.textContent = "Usuário ou senha inválidos.";
+  }
+}
